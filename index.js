c64bdf954f1cb49ffc5e04ac9141d897d503ab2c
bwilson19 <brendan.wilson19@gmail.com>
2020-02-06 12:56:11 -0800

    minor edits

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs');
 
 app.use(methodOverride('_method'));
 
-app.use('/question', questionController); //auto includes /todos in routes
+app.use('/question', questionController);
 app.listen(4000, () => {
   console.log('app listening on port 4000');
 });
